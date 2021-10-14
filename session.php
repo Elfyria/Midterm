f41c5307f8e4c86961bf2e77c05575a6b3e9803f
QuentinRoa <90809562+QuentinRoa@users.noreply.github.com>
2021-10-14 16:45:06 -0400

    Update session.php
    less basic, sets session details.

--- session.php
+++ session.php
@@ -1,3 +1,11 @@
 <?php
 require_once "csv_util.php";
-if(checkUser($_GET["uname"],$_GET["pword"])) session_start();
+session_start();
+if(checkUser($_GET["uname"],$_GET["pword"])){ 
+    $_SESSION["uname"]=$_GET["uname"];
+    $_SESSION["pword"]=$_GET["pword"];
+    $_SESSION["mod"]=true;}
+else{
+    $_SESSION["uname"]="undefined";
+    $_SESSION["pword"]="";
+    $_SESSION["mod"]=false;}
